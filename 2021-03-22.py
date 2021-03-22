989c48f35e1d1a3ead24b1babd9685bfb61f2b78
Paul Go <hygjob@gmail.com>
2021-03-22 13:20:37 +0900

    Create 2021-03-22.py

---
+++ 2021-03-22.py
@@ -0,0 +1,22 @@
+# 2021.3.22
+# 정수 내림차순으로 배치하기
+# bubble sort 사용
+
+def solution(n):
+    answer = 0    
+    sn = str(n)    
+    arr = []
+    for i in range(len(sn)):
+        arr.append(int(sn[i]))
+    
+    for i in range(len(arr) - 1, 0, -1):
+        for j in range(i):
+            if arr[j] < arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    s = ''
+    for i in range(len(arr)):
+        s = s + str(arr[i])
+        
+    answer = int(s)
+    
+    return answer
